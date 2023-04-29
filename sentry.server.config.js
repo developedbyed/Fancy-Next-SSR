8ae540931ccf422bb9e28dff65e72c5e80be1a57
developedbyed <ed.magician@gmail.com>
2023-04-29 11:45:24 +0100

    sentry dsn

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/nextjs"
 import { ProfilingIntegration } from "@sentry/profiling-node"
 
 Sentry.init({
+  dsn: "https://7e2b56737a4b4703a0c7e2c21254d099@o4505096963031040.ingest.sentry.io/4505096965652480",
   // ... SDK config
   traceSampleRate: 1.0,
   profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
