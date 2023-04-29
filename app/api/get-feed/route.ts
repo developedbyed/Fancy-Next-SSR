797b54b3a90bf79429f0d0011e5ac4ad5bc2036b
developedbyed <ed.magician@gmail.com>
2023-04-29 16:26:42 +0100

    fix

--- app/api/get-feed/route.ts
+++ app/api/get-feed/route.ts
@@ -5,15 +5,15 @@ export const revalidate = 0
 
 export async function GET(req: Request) {
   try {
-    const posts = await prisma.post
+    const { data, info } = await prisma.post
       .findMany({
         include: { author: true },
         orderBy: { createdAt: "asc" },
         cacheStrategy: { swr: 60, ttl: 60 },
       })
       .withAccelerateInfo()
-
-    return NextResponse.json(posts)
+    console.log(info)
+    return NextResponse.json(data)
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: "Something went wrong ❣️" })
