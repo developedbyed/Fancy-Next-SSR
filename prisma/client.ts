3f0f3c5fc4cfb95296cc97e16b2ca79d9ae40e92
developedbyed <ed.magician@gmail.com>
2023-04-29 16:12:38 +0100

    accelerate hope

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from "@prisma/client"
 import useAccelerate from "@prisma/extension-accelerate"
 
-export const prisma = new PrismaClient({}).$extends(useAccelerate)
+export const prisma = new PrismaClient().$extends(useAccelerate)
