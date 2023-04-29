bbb3727b9d619a74ec17cec79237040b3258a5ba
developedbyed <ed.magician@gmail.com>
2023-04-29 22:55:52 +0100

    ttl1 swr1

--- app/api/get-feed/route.ts
+++ app/api/get-feed/route.ts
@@ -7,18 +7,17 @@ import { prisma } from "../../../prisma/client"
 export async function GET(req: Request) {
   const startTime = Date.now()
   try {
-    const { data, info } = await prisma.post
+    const posts = await prisma.post
       .findMany({
         include: { author: true },
         orderBy: { createdAt: "asc" },
-        cacheStrategy: { swr: 1 },
+        cacheStrategy: { swr: 1, ttl: 1 },
       })
       .withAccelerateInfo()
-    console.dir(data)
-    console.dir(info)
+
     console.log("Request took:", Date.now() - startTime, "ms")
 
-    return NextResponse.json(data)
+    return NextResponse.json(posts)
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: "Something went wrong ❣️" })
