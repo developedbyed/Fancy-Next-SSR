afbece977b6d76e774e89e123af1f25c166bf7d8
developedbyed <ed.magician@gmail.com>
2023-05-01 16:59:37 +0100

    done

--- app/api/get-feed/route.ts
+++ app/api/get-feed/route.ts
@@ -1,15 +1,11 @@
 import { NextResponse } from "next/server"
 import { prisma } from "../../../prisma/client"
 
-export const runtime = "edge"
-export const preferredRegion = "sfo1"
-
 export async function GET(req: Request) {
   try {
     const posts = await prisma.post.findMany({
       include: { author: true },
       orderBy: { createdAt: "asc" },
-      cacheStrategy: { swr: 5, ttl: 10 },
     })
 
     return NextResponse.json(posts)
--- prisma/client.ts
+++ prisma/client.ts
@@ -1,16 +1,9 @@
-import { PrismaClient } from "@prisma/client/edge"
-import useAccelerate from "@prisma/extension-accelerate"
-
-function makePrisma() {
-  return new PrismaClient({
-    datasources: { db: { url: process.env.ACCELERATE_URL } },
-  }).$extends(useAccelerate)
-}
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as {
-  prisma: ReturnType<typeof makePrisma>
+  prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? makePrisma()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = makePrisma()
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
